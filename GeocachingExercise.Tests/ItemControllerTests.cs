ed83b97ae13f887dea8be73db8cb8df1b4ed5b37
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-06-14 16:53:19 -0400

    set up test proj and repo

---
+++ GeocachingExercise.Tests/ItemControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GeocachingExercise.Tests
+{
+    public class ItemControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ GeocachingExercise.Tests/Mocks/InMemoryItemRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeocachingExercise.Models;
+using GeocachingExercise.Repositories;
+
+namespace GeocachingExercise.Tests.Mocks
+{
+    internal class InMemoryItemRepository : IItemRepository
+    {
+        private readonly List<Item> _data;
+
+        public List<Item> InternalData
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        public InMemoryItemRepository(List<Item> startingData)
+        {
+            _data = startingData;
+        }
+
+        public List<Item> GetActiveItems(DateTime today, int cacheId)
+        {
+            return _data.Where(i => (DateTime.Parse(i.ActiveStartDate) <= today) && (DateTime.Parse(i.ActiveEndDate) >= today)).ToList();
+        }
+
+        public Item getItemById( int id)
+        {
+            return _data.FirstOrDefault(i => i.Id == id);
+        }
+
+        public void AddItem(Item item)
+        {
+            var lastItem = _data.Last();
+            item.Id = lastItem.Id + 1;
+            _data.Add(item);
+        }
+
+        public void MoveItem(int itemId, int? newCacheId )
+        {
+            Item item = _data.FirstOrDefault(i => i.Id == itemId);
+            item.CacheId = newCacheId;
+        }
+
+        public int CacheItemCount(int? id)
+        {
+            return _data.Count(i => i.CacheId == id);
+        }
+    }
+}
